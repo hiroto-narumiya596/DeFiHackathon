bc3206e36a3442a21de2b7bdbfb842865a0201f8
ryoheiomine <omine-ryohei@g.ecc.u-tokyo.ac.jp>
2023-12-10 22:10:00 +0900

    updated chain.ts

--- client/app/api/api_chain/chain.ts
+++ client/app/api/api_chain/chain.ts
@@ -3,6 +3,24 @@
 import * as xrpl from '@transia/xrpl'
 import * as rippleAddressCodec from 'ripple-address-codec'
 
+//@ts-ignore
+import * as xfl from '@xrplkit/xfl'
+
+function xflToHex(xflStr: string): string {
+    let buf = Buffer.alloc(8)
+
+    buf.writeBigInt64LE(xfl.toBigInt(xflStr))
+    return buf.toString('hex').toUpperCase()
+}
+
+export function bi64ToHex(bi64: bigint): string {
+    let buf = Buffer.alloc(8)
+
+    buf.writeBigInt64LE(bi64)
+    return buf.toString('hex').toUpperCase()
+}
+
+
 type LoginState = "None" | "Trier" | "Checker"
 
 const tokenCode = "TCS"
@@ -55,8 +73,12 @@ export class Chain {
         return balance
     }
     async loadLastCommitmentId() : Promise<string | undefined> {
-        const trierKey = "" // TODO
-        const namespaceId = "tacs" // TODO
+        if (this.wallet === undefined) {
+            return undefined
+        }
+
+        const trierKey = "B000000000000000" + rippleAddressCodec.decodeAccountID(this.wallet.classicAddress)
+        const namespaceId = "tacs"
 
         const response = await this.client!.request({
             "command": "ledger_entry",
@@ -68,7 +90,7 @@ export class Chain {
         }) as xrpl.LedgerEntryResponse
         return "" // TODO
     }
-    async commit(commitId: string) : Promise<boolean> {
+    async createTask(taskHash: string, intervalInSeconds: string) : Promise<boolean> {
         if (this.wallet === undefined || this.client === undefined) {
             return false
         }
@@ -77,15 +99,116 @@ export class Chain {
             TransactionType: "Invoke",
             Account: this.wallet!.classicAddress,
             Destination: this.issuerAccount,
+            HookParameters: [
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("0"),
+                        HookParameterValue: xrpl.convertStringToHex("T")
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("H"),
+                        HookParameterValue: taskHash 
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("T"),
+                        HookParameterValue: bi64ToHex(BigInt(intervalInSeconds))
+                    }
+                },
+            ]
         }
         const preparedInvokeTx = await this.client!.autofill(invokeTx)
         const signedInvokeTx = this.wallet.sign(preparedInvokeTx)
+        const invokeTxResult = await this.client!.submitAndWait(signedInvokeTx.tx_blob)
+
+        return true // TODO: check the result
+    }
+    async commit(taskId: string, lockAmount: string, isXrp: boolean) : Promise<boolean> {
+        if (this.wallet === undefined || this.client === undefined) {
+            return false
+        }
+
+        const payAmount = isXrp ? xrpl.xrpToDrops(lockAmount) : { "currency": tokenCode, "issuer": this.issuerAccount, "value": lockAmount}
+        const paymentTx: xrpl.Payment = {
+            TransactionType: "Payment",
+            Account: this.wallet!.classicAddress,
+            Destination: this.issuerAccount,
+            Amount: payAmount,
+        }
+        const preparedPaymentTx = await this.client!.autofill(paymentTx)
+        const signedPaymentTx = this.wallet.sign(preparedPaymentTx)
+        const paymentTxResult = await this.client!.submitAndWait(signedPaymentTx.tx_blob)
+        // TODO: check the result 
+
+        const invokeTx: xrpl.Invoke = {
+            TransactionType: "Invoke",
+            Account: this.wallet!.classicAddress,
+            Destination: this.issuerAccount,
+            HookParameters: [
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("0"),
+                        HookParameterValue: xrpl.convertStringToHex("C")
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("I"),
+                        HookParameterValue: taskId, 
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("L"),
+                        HookParameterValue: xflToHex(xfl.XFL(String(Number(lockAmount) * (-1)))), // In xfl library, sign of a number is opposite
+                    }
+                },
+            ]
+        }
+        const preparedInvokeTx = await this.client!.autofill(invokeTx)
+        const signedInvokeTx = this.wallet.sign(preparedInvokeTx)
+        const invokeTxResult = await this.client!.submitAndWait(signedInvokeTx.tx_blob)
 
         return true // TODO: check the result of tx
     }
     async evaluateCommit(commitId: string, isApproval: boolean) : Promise<boolean> {
-        // TODO
-        return true
+        if (this.wallet === undefined || this.client === undefined) {
+            return false
+        }
+
+        const invokeTx: xrpl.Invoke = {
+            TransactionType: "Invoke",
+            Account: this.wallet!.classicAddress,
+            Destination: this.issuerAccount,
+            HookParameters: [
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("0"),
+                        HookParameterValue: xrpl.convertStringToHex("E")
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("I"),
+                        HookParameterValue: commitId 
+                    }
+                },
+                {
+                    HookParameter: {
+                        HookParameterName: xrpl.convertStringToHex("R"),
+                        HookParameterValue: bi64ToHex(isApproval ? 1n : 2n)
+                    }
+                },
+            ]
+        }
+        const preparedInvokeTx = await this.client!.autofill(invokeTx)
+        const signedInvokeTx = this.wallet.sign(preparedInvokeTx)
+        const invokeTxResult = await this.client!.submitAndWait(signedInvokeTx.tx_blob)
+
+        return true // TODO: check the result
     }
 
     static getCheckerIdFromTaskId(taskId: string) : string | undefined {
